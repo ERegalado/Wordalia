77a530770ff8f46ae49238cc23242d8282b2efbe
ERegalado <rega_omega@hotmail.com>
2016-10-15 20:16:29 +0200

    TEST 2 - Timezone.

--- be/application/controllers/wordalia.php
+++ be/application/controllers/wordalia.php
@@ -21,7 +21,7 @@ class Wordalia extends REST_Controller
     }
 	
 		
-	function index_get(){echo "Current time is: ".date('Y-m-d H:i:s'); echo 'date_default_timezone_set: ' . date_default_timezone_get() . '<br />';}
+	function index_get(){echo "Current time is: ".date('Y-m-d H:i:s'); echo 'default timezone: ' . date_default_timezone_get() . '<br />';}
     
 	    
     function word_get()
