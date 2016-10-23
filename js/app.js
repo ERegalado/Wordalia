8c276d491c1a31cad3d055cf090aa395a24a99b4
ERegalado <rega_omega@hotmail.com>
2016-10-23 18:18:31 +0200

    Dynamic recognition of base url in JS.

--- js/app.js
+++ js/app.js
@@ -3,16 +3,9 @@
   app.controller('WDController', function($http){
 	var wdVault = this;
 	wdVault.wd = []
-	wdVault.hello = '';
-    $http.get('http://localhost/wordalia/be/index.php/wordalia/word') //LOcal
-    // $http.get('http://wordalia.azurewebsites.net/be/index.php/wordalia/word') //Azure
-		.success(function(data) {
-			wdVault.wd = data.word;
-			wdVault.hello = 'Hola';
-			console.log(wdVault.wd);
-			console.log('head' + wdVault.head);
-        });
-		console.log('1 '+wdVault.wd);
+    // $http.get('http://localhost/wordalia/be/index.php/wordalia/word') //LOcal
+    $http.get(window.location.href+'be/index.php/wordalia/word') //Azure
+		.success(function(data) {wdVault.wd = data.word;});
   });
   
 })();
